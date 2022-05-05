302304de454ee813d6945eb5b0b10b0a65e4e32d
Arruda Junior <juniorarruda15@gmail.com>
2022-05-05 19:20:50 -0300

    Exercício 019
    Criando um programa que sorteei uma pessoa aleatória entre quatro.

---
+++ PythonExercicios/ex019.py
@@ -0,0 +1,8 @@
+from random import choice
+al1 = input('Primeiro aluno: ')
+al2 = input('Segundo aluno: ')
+al3 = input('Terceiro aluno: ')
+al4 = input('Quarto aluno: ')
+lista = [al1, al2, al3, al4]
+escolhido = choice(lista)
+print('O aluno escolhido foi {}'.format(escolhido))
