9e91fae8a989778f57aed647a578e5c85a35c3e9
Arruda Junior <juniorarruda15@gmail.com>
2022-05-05 11:49:39 -0300

    Pratica da aula 08 módulos
    Arquivos da pratica da aula 08.

---
+++ python_teste/aula08a.py
@@ -0,0 +1,21 @@
+import emoji as emoji
+
+import emoji
+print(emoji.emojize('Olá,Mundo :earth_americas:', use_aliases=True))
+
+
+# from math import sqrt, floor
+
+# import random
+# num = random.randint(1, 10)
+# print(num)
+
+# import math
+# num = int(input('Digite um número: '))
+# raiz = sqrt(num)
+# print('A raiz de {} é igual a {}'.format(num, math.ceilraiz))
+# print('A raiz de {} é igual a {}'.format(num, raiz))
+# print('A raiz de {} é igual a {}'.format(num, floor(raiz)))
+
+
+
