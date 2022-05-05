8de7ea381f511893827102c451e8a7f3d632e658
Arruda Junior <juniorarruda15@gmail.com>
2022-05-05 13:23:02 -0300

    Exercícios 16
    Resolução do exercício

---
+++ PythonExercicios/ex016.py
@@ -0,0 +1,8 @@
+'''from math import trunc
+# import math
+num = float(input('Digite um número: '))
+# numint = math.trunc(num)
+print('O número {} tem a parte inteira {}'.format(num, trunc(num)))'''
+
+num = float(input('Digite um númeor: '))
+print('O valor Digitado foi {} e a sua porção inteira é {}'.format(num, int(num)))
