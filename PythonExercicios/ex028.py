3a13df1095861237d580b89fd7d80a8606f7d33e
Arruda Junior <juniorarruda15@gmail.com>
2022-05-07 19:15:22 -0300

    Exercício 028
    Jogo da Adivinhação v1.0

---
+++ PythonExercicios/ex028.py
@@ -0,0 +1,15 @@
+from random import randint
+from time import sleep
+computador = randint(0, 5) # faz o sorteio entre o numero 0 e 5.
+print('-=-' * 19)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 19)
+jogador = int(input('Em que número eu pensei? ')) # Jogador digita tentando adivinhar.
+print('PROCESSANDO...')
+sleep(2)
+if jogador == computador:
+    print('\nPARABÉNS! Você acertou o numero escolhido foi {}'.format(computador))
+else:
+    print('\nErrou! o número sorteado foi {} e não {}!'.format(computador, jogador))
+    print('Tente de novo na proxima vez kkkkk')
+print('\nAté a proxima!')
