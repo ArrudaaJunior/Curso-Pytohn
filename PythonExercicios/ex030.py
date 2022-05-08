480a353631676933df1ff70d80bc70378840ddec
Arruda Junior <juniorarruda15@gmail.com>
2022-05-08 14:29:10 -0300

    Exercício Python 30
    programa que ler um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.

---
+++ PythonExercicios/ex030.py
@@ -0,0 +1,14 @@
+num = int(input('Me diga um número qualquer: '))
+if (num / 2) % 1:
+    print('O número {} é IMPAR'.format(num))
+else:
+    print('O número é {} PAR'.format(num))
+
+# Outra forma de fazer o programa
+
+'''numero = int(input('Me diga qualquer número: '))
+resultado = numero % 2
+if resultado == 0:
+    print('O número {} é PAR'.format(numero))
+else:
+    print('O número {} é ÍMPAR'.format(numero))'''
