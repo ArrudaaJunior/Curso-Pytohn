e0de925217c3963715a90ed6da27c688e9ad75a5
Arruda Junior <juniorarruda15@gmail.com>
2022-05-08 23:27:50 -0300

    Aula 011 - Cores no terminal
    utilizando os códigos de escape sequence ANSI para configurar cores para os programas em Python. utilizando o código \033[m com todas as suas principais possibilidades.

---
+++ python_teste/aula11a.py
@@ -0,0 +1,12 @@
+print('\033[;32;40mOlá, Mundo!\033[m')
+a = 3
+b = 5
+print('Os valores são \033[32m{}\033[m e \033[31m{}\033[m !!!'.format(a, b))
+nome = 'Júnior'
+print('Olá! Muito prazer em te conhecer, {}{}{}!!!'.format('\033[4;34m', nome, '\033[m'))
+cores = {'limpa' : '\033[m',
+         'azul': '\033[34m',
+         'amarelo' :'\033[34m',
+         'pretoebranco':'\033[7;30m'}
+print('Olá, Muito  prazer {}{}{}!!!'.format(cores['pretoebranco'], nome, cores['limpa']))
+
